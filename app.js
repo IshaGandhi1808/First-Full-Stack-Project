a8df3060b373d1bc70e05915353783b29cd7d360
IshaGandhi1808 <gandhiisha188@gmail.com>
2026-05-02 00:03:29 +0530

    Use connect-mongo session store

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(cors());
 const ExpressError = require("./utils/ExpressError");
 
 const session = require("express-session");
+const { MongoStore } = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -48,10 +49,25 @@ async function main() {
   await mongoose.connect(dbUrl);
 }
 
+// Use connect-mongo package for production
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("Error in Mongo Session Store", err);
+});
+
 // Use express-session package
 
 let sessionOptions = {
-  secret: "sessioncode",
+  store: store,
+  secret: process.env.SECRET,
   resave: true,
   saveUninitialized: true,
 };
